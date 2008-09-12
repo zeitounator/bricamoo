f9b2c1938368e393e1faa0ecf2168e442a7fc5f6
Olivier Clavel <olivier.clavel@gmail.com>
2008-09-12 22:17:07 +0000

    New bricamoo: bmMenu for dynamic menu

---
+++ javascript/menu.js
@@ -0,0 +1,16 @@
+/**
+ * A class to handle a dynamic menu
+ * @copyright Olivier Clavel (olivier DOT clavel AT gmail DOT com)
+ */
+var bmMenu = new Class(
+	
+	initialize: function(element, config) {
+	}
+
+	defaultConfig: {
+		'delay': 500
+	}
+
+	parseConfig: {
+	}
+);
