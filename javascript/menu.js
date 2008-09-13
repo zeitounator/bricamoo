1af02667524fec1cb2a6bb6b7ce7c75a16da32b5
Olivier Clavel <olivier.clavel@gmail.com>
2008-09-13 00:25:19 +0000

    first drafs of a kinda working menu

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,16 +1,46 @@
 /**
+ * Bricamoo menu
+ *
  * A class to handle a dynamic menu
  * @copyright Olivier Clavel (olivier DOT clavel AT gmail DOT com)
  */
-var bmMenu = new Class(
+var bmMenu = new Class({
 	
-	initialize: function(element, config) {
-	}
+	Implements: [Options],
 
-	defaultConfig: {
-		'delay': 500
-	}
+	options: {
+		duration: 500,
+		delay: 100,
+	},
+	
+	initialize: function(element, options) {
+		this.setOptions(options);
+		this.parseMenu(element);
+	},
 
-	parseConfig: {
+	parseMenu: function(element) {
+		/* Get all second level menus, set absolute display and fade to 0 */	
+		var secondLevel = element.getElements('dd');
+		secondLevel.setStyle('position', 'absolute');
+		secondLevel.setStyle('opacity', 0);
+
+		/* Get all top level entries, set the cursor on mouseover */
+		var firstLevel = element.getElements('dt');
+		firstLevel.setStyle('cursor', 'pointer');
+
+		secondLevel.each(function(menu) {
+			var title = menu.getPrevious('dt');
+			menu.set('tween', {
+				duration: this.options.duration, 
+				transition: Fx.Transitions.Quart.easeInOut
+			});
+ 
+			title.addEvent('mouseenter', function() {
+				menu.tween('opacity', 0, 1).delay(this.options.delay);
+			}),
+			title.addEvent('mouseleave', function() {
+				menu.tween('opacity', 1, 0).delay(this.options.delay);
+			});
+		}, this);
 	}
-);
+});
